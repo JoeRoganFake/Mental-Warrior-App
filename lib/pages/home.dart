90496232534a63d7d8aa98bddbf3d611d4a7abd2
PC <savkaondrej@gmail.com>
2025-01-07 12:42:23 +0100

    added a button to the bottom right with a list of actions to add and its awesome

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,6 +21,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        splashColor: Colors.blue,
+        onPressed: () {
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(
+              MediaQuery.of(context).size.width - 5,
+              MediaQuery.of(context).size.height - 200,
+              20,
+              0,
+            ),
+            items: [
+              PopupMenuItem<String>(
+                value: 'task',
+                child: Text('Task'),
+                onTap: () => addTaskDialog(context),
+              ),
+              PopupMenuItem<String>(
+                value: 'habit',
+                child: Text('Habit'),
+              ),
+            ],
+          );
+        },
+        backgroundColor: const Color.fromARGB(255, 103, 113, 121),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
       backgroundColor: Colors.white,
       body: ListView(
         children: [
@@ -38,102 +68,10 @@ class _HomePageState extends State<HomePage> {
                 "Tasks Today",
                 textAlign: TextAlign.start,
               ),
-              Column(
-                children: [
-                  _taskList(),
-                  FloatingActionButton(
-                    splashColor: Colors.blue,
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => SimpleDialog(
-                          children: [
-                            Form(
-                                key: _formKey,
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Text(
-                                      "New Task",
-                                    ),
-                                    TextFormField(
-                                      controller: _labelController,
-                                      autofocus: true,
-                                      validator: (value) {
-                                        if (value!.isEmpty) {
-                                          return "* Field Is Required";
-                                        }
-                                        return null;
-                                      },
-                                      decoration: InputDecoration(
-                                          hintText: "Label",
-                                          prefixIcon: const Icon(Icons.label),
-                                          border: InputBorder.none),
-                                    ),
-                                    TextFormField(
-                                      controller: _descriptionController,
-                                      maxLines: null,
-                                      keyboardType: TextInputType.multiline,
-                                      decoration: InputDecoration(
-                                          hintText: "Description",
-                                          prefixIcon:
-                                              const Icon(Icons.description),
-                                          border: InputBorder.none),
-                                    ),
-                                    TextFormField(
-                                      controller: _dateController,
-                                      onTap: () {
-                                        Functions.dateAndTimePicker(
-                                            context, _dateController);
-                                      },
-                                      readOnly: true,
-                                      decoration: InputDecoration(
-                                          hintText: "Due To",
-                                          prefixIcon:
-                                              const Icon(Icons.calendar_month),
-                                          border: InputBorder.none),
-                                    ),
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        _formKey.currentState!.validate();
-                                        _databaseService.addTask(
-                                            _labelController.text,
-                                            _dateController.text,
-                                            _descriptionController.text);
-                                        _dateController.clear();
-                                        _labelController.clear();
-                                        _descriptionController.clear();
-                                        Navigator.pop(context);
-                                        setState(() {});
-                                      },
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          const Icon(Icons.create_outlined),
-                                          Text(
-                                            textAlign: TextAlign.center,
-                                            "Add Task",
-                                            style: TextStyle(),
-                                          )
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ))
-                          ],
-                        ),
-                      );
-                    },
-                    backgroundColor: const Color.fromARGB(255, 103, 113, 121),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+              const SizedBox(
+                height: 10,
               ),
+              _taskList(),
             ],
           ),
         ],
@@ -141,39 +79,86 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Container _taskListOr() {
-    return Container(
-      color: Colors.lightBlueAccent,
-      height: 105,
-      width: 300,
-      child: ListView.separated(
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) => Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: const Color.fromARGB(255, 119, 119, 119),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("DUMMY"),
-                    Checkbox(value: true, onChanged: null),
-                  ],
-                ),
-              ),
-          separatorBuilder: (context, index) => const SizedBox(
-                width: 25,
-                height: 5,
-              ),
-          itemCount: 5),
+  Future<dynamic> addTaskDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        children: [
+          Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "New Task",
+                  ),
+                  TextFormField(
+                    controller: _labelController,
+                    autofocus: true,
+                    validator: (value) {
+                      if (value!.isEmpty || value == "") {
+                        return "     *Field Is Required";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Label",
+                        prefixIcon: const Icon(Icons.label),
+                        border: InputBorder.none),
+                  ),
+                  TextFormField(
+                    controller: _descriptionController,
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                        hintText: "Description",
+                        prefixIcon: const Icon(Icons.description),
+                        border: InputBorder.none),
+                  ),
+                  TextFormField(
+                    controller: _dateController,
+                    onTap: () {
+                      Functions.dateAndTimePicker(context, _dateController);
+                    },
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        hintText: "Due To",
+                        prefixIcon: const Icon(Icons.calendar_month),
+                        border: InputBorder.none),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _databaseService.addTask(_labelController.text,
+                            _dateController.text, _descriptionController.text);
+                        _dateController.clear();
+                        _labelController.clear();
+                        _descriptionController.clear();
+                        Navigator.pop(context);
+                        setState(() {});
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.create_outlined),
+                        Text(
+                          textAlign: TextAlign.center,
+                          "Add Task",
+                          style: TextStyle(),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
     );
   }
 
   Widget _taskList() {
     return Container(
-      color: const Color.fromARGB(255, 250, 250, 250),
       height: 300,
       width: 200,
       child: FutureBuilder(
@@ -187,6 +172,7 @@ class _HomePageState extends State<HomePage> {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: GestureDetector(
+                    onTap: () {},
                     onLongPress: () {
                       _databaseService.deleteTask(task.id);
                       setState(() {});
