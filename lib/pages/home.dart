26f0d2ea00b895ccee117cab15208527a565ac02
PC <savkaondrej@gmail.com>
2025-01-21 18:32:23 +0100

    added funcioning completed tasks list,need to implement checkbox functions

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
   final _descriptionController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final DatabaseService _databaseService = DatabaseService.instace;
-  bool _compTaskVisible = false;
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -80,188 +80,52 @@ class _HomePageState extends State<HomePage> {
                 child: FutureBuilder(
                     future: _databaseService.getCompletedTasks(),
                     builder: (context, snapshot) {
-                      return ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        itemCount: snapshot.data?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          Task task = snapshot.data![index];
-                          return Padding(
-                            padding: const EdgeInsets.all(6.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                _labelController.text = task.label;
-                                _descriptionController.text = task.description;
-                                _dateController.text = task.deadline;
-                                showDialog(
-                                  context: context,
-                                  builder: (context) => SimpleDialog(
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                          Form(
-                                            key: _formKey,
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.spaceEvenly,
-                                              children: [
-                                                Padding(
-                                                  padding:
-                                                      const EdgeInsets.all(8.0),
-                                                  child: Text(
-                                                    "Edit Task",
-                                                  ),
-                                                ),
-                                                TextFormField(
-                                                  controller: _labelController,
-                                                  validator: (value) {
-                                                    if (value!.isEmpty ||
-                                                        value == "") {
-                                                      return "     *Field Is Required";
-                                                    }
-                                                    return null;
-                                                  },
-                                                  decoration: InputDecoration(
-                                                      hintText: "Label",
-                                                      prefixIcon: const Icon(
-                                                          Icons.label),
-                                                      border: InputBorder.none),
-                                                ),
-                                                TextFormField(
-                                                  controller:
-                                                      _descriptionController,
-                                                  maxLines: null,
-                                                  keyboardType:
-                                                      TextInputType.multiline,
-                                                  decoration: InputDecoration(
-                                                      hintText: "Description",
-                                                      prefixIcon: const Icon(
-                                                          Icons.description),
-                                                      border: InputBorder.none),
-                                                ),
-                                                TextFormField(
-                                                  controller: _dateController,
-                                                  onTap: () {
-                                                    Functions.dateAndTimePicker(
-                                                        context,
-                                                        _dateController);
-                                                  },
-                                                  readOnly: true,
-                                                  decoration: InputDecoration(
-                                                      hintText: "Due To",
-                                                      prefixIcon: const Icon(
-                                                          Icons.calendar_month),
-                                                      border: InputBorder.none),
-                                                ),
-                                                ElevatedButton(
-                                                  onPressed: () {
-                                                    if (_formKey.currentState!
-                                                        .validate()) {
-                                                      _databaseService
-                                                          .updateTask(
-                                                              task.id,
-                                                              "label",
-                                                              _labelController
-                                                                  .text);
-                                                      _databaseService.updateTask(
-                                                          task.id,
-                                                          "description",
-                                                          _descriptionController
-                                                              .text);
-                                                      _databaseService
-                                                          .updateTask(
-                                                              task.id,
-                                                              "deadline",
-                                                              _dateController
-                                                                  .text);
+                      List<Task> tasks = snapshot.data!;
 
-                                                      Navigator.pop(context);
-                                                      setState(() {});
-                                                    }
-                                                  },
-                                                  child: Row(
-                                                    mainAxisAlignment:
-                                                        MainAxisAlignment
-                                                            .center,
-                                                    children: [
-                                                      Padding(
-                                                        padding:
-                                                            const EdgeInsets
-                                                                .all(8.0),
-                                                        child: const Icon(Icons
-                                                            .add_task_outlined),
-                                                      ),
-                                                      Text(
-                                                        textAlign:
-                                                            TextAlign.center,
-                                                        "Edit Task",
-                                                        style: TextStyle(),
-                                                      )
-                                                    ],
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      )
-                                    ],
+                      return SingleChildScrollView(
+                        child: ExpansionPanelList(
+                          expansionCallback: (int index, bool isExpanded) {
+                            setState(() {
+                              _isExpanded = !_isExpanded;
+                            });
+                          },
+                          children: [
+                            ExpansionPanel(
+                              headerBuilder: (context, isExpanded) {
+                                return ListTile(
+                                  title: Text(
+                                    "Completed Tasks",
                                   ),
-                                ).then((_) {
-                                  Future.delayed(
-                                      const Duration(milliseconds: 100), () {
-                                    _labelController.clear();
-                                    _descriptionController.clear();
-                                    _dateController.clear();
-                                  });
-                                });
+                                );
                               },
-                              onLongPress: () {
-                                _databaseService.deleteTask(task.id);
-                                setState(() {});
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color:
-                                      const Color.fromARGB(255, 119, 119, 119),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Flexible(
-                                      child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 30),
-                                        child: Text(
-                                          task.label,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                        ),
-                                      ),
+                              body: Column(
+                                children: tasks.map((task) {
+                                  return ListTile(
+                                    title: Text(task.label,
+                                        style: TextStyle(color: Colors.white)),
+                                    tileColor: const Color.fromARGB(
+                                        255, 119, 119, 119),
+                                    trailing: Checkbox(
+                                      value: task.status == 0,
+                                      onChanged: (value) {
+                                        _databaseService.updateTaskStatus(
+                                          task.id,
+                                          value == true ? 1 : 0,
+                                        );
+                                        setState(() {});
+                                      },
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(right: 30),
-                                      child: Checkbox(
-                                        value: task.status == 1,
-                                        onChanged: (value) {
-                                          _databaseService.updateTaskStatus(
-                                              task.id, value == true ? 1 : 0);
-
-                                          setState(() {});
-                                        },
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                    onLongPress: () {
+                                      _databaseService.deleteCompTask(task.id);
+                                      setState(() {});
+                                    },
+                                  );
+                                }).toList(),
                               ),
+                              isExpanded: _isExpanded,
                             ),
-                          );
-                        },
+                          ],
+                        ),
                       );
                     }),
               )
